1117d3d773a763ef97510c72c95eca41c9d89362
mukha <mukhammedismailov96@gmail.com>
2024-10-11 11:24:47 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import numpy as np
+from  functions import *
+from config import *
+from analyze_clusters import Analayze
+
+images = os.listdir(path_to_images)
+
+for i in images:
+
+    img_name = os.path.join(path_to_images,i)
+    img = cv.imread(img_name)
+
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    #get all dark objects because mostly back ground and eritrocytes is lightt color
+    img_gray[img_gray >  img_gray.mean()] = 0
+    img_gray[img_gray != 0] = 255
+
+    #find max area object wich mostly is leyko cell and deliver its from original image
+    img_max_area = find_max_area(img_gray)
+    object_img = get_cell_img(img_max_area, img)
+
+
+    cluster_img, color_matrix = claster_cell_img(object_img, 3, returnAsGray=True)
+
+
+    analize_img = Analayze(color_matrix, cluster_img)
+    cores = analize_img.get_cores()
+    cytopasms = analize_img.get_cyto()
+
+    tot = cores*cytopasms
+
+    img_dizi = [img, tot]
+    plot_images(img_dizi)
+
+    #break
